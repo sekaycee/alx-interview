acd8c256c85192c9f75cd20bdd751112700533f8
Kay Cee <se.kaycee@gmail.com>
2023-07-25 00:01:24 +0100

    feat: solve task 0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' task 0: UTF-8 Validation '''
+
+
+def validUTF8(data):
+    ''' determine if a given data set represents a valid UTF-8 encoding '''
+    count = 0
+
+    for x in data:
+        if 191 >= x >= 128:
+            if not count:
+                return False
+            count -= 1
+        else:
+            if count:
+                return False
+            if x < 128:
+                continue
+            elif x < 224:
+                count = 1
+            elif x < 240:
+                count = 2
+            elif x < 248:
+                count = 3
+            else:
+                return False
+    return count == 0
