c77bced937c2f5cbb9acb0a1c7de90dfc4ffa0ea
Kay Cee <se.kaycee@gmail.com>
2023-09-05 20:15:05 +0100

    fix: edit variable names

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,7 +6,7 @@ def isWinner(x, nums):
     ''' determine the winner of a prime game session with `x` rounds. '''
     if x < 1 or not nums:
         return None
-    marias_wins, bens_wins = 0, 0
+    maria_wins, ben_wins = 0, 0
 
     # generate primes with a limit of the maximum number in nums
     n = max(nums)
@@ -21,8 +21,8 @@ def isWinner(x, nums):
     # filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
-        bens_wins += primes_count % 2 == 0
-        marias_wins += primes_count % 2 == 1
-    if marias_wins == bens_wins:
+        ben_wins += primes_count % 2 == 0
+        maria_wins += primes_count % 2 == 1
+    if maria_wins == ben_wins:
         return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
